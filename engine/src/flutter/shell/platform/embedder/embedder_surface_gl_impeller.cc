f6645428dd4719f1f3bdf48b5fe5d02388de1be2
Doctor Logix <doctor.logix@gmail.com>
2026-09-13 10:45:06 +0200

    Release embedder resource contexts on shutdown

--- engine/src/flutter/shell/platform/embedder/embedder_surface_gl_impeller.cc
+++ engine/src/flutter/shell/platform/embedder/embedder_surface_gl_impeller.cc
@@ -229,4 +229,10 @@ sk_sp<GrDirectContext> EmbedderSurfaceGLImpeller::CreateResourceContext()
   return nullptr;
 }
 
+// |EmbedderSurface|
+void EmbedderSurfaceGLImpeller::ReleaseResourceContext() const {
+  worker_->SetReactionsAllowedOnCurrentThread(false);
+  gl_dispatch_table_.gl_clear_current_callback();
+}
+
 }  // namespace flutter
